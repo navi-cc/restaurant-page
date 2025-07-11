ff3d35ab09980c6b99b691b3b8e442d73bb01c1a
navi-cc <157870465+navi-cc@users.noreply.github.com>
2025-07-11 15:44:37 +0800

    Add navigation handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import "./styles.css";
 
-console.log("test");
+const nav = document.querySelector("header > nav")
+const mainContent = document.querySelector('#content')
+
+nav.onclick = navigationHandler;
+
+function navigationHandler(e) {
+
+    if (e.target.nodeName !== "LI") return;
+
+    const target = e.target.textContent;
+    mainContent.textContent = ''
+    
+  
+    switch (target) {
+        case "home":
+        
+            e.target.classList.add("cover");
+            e.target.classList.remove("hidden")
+            nav.children[1].classList.add("hidden")
+            nav.children[2].classList.add("hidden")
+            
+            break;
+        
+        case "menu":
+
+            e.target.classList.add("cover");
+            e.target.classList.remove("hidden")
+            nav.children[0].classList.add("hidden")
+            nav.children[2].classList.add("hidden")
+            break;   
+
+        case "about":
+
+            
+            e.target.classList.add("cover");
+            e.target.classList.remove("hidden")
+            nav.children[0].classList.add("hidden")
+            nav.children[1].classList.add("hidden")
+            break;   
+    
+        default:
+            break;
+    }
+    
+}
